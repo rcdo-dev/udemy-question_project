9a6838052e4b1034543b230ff3e28048b9bf61d4
rcdo-dev <rcdo.dev@gmail.com>
2024-03-23 14:24:20 -0300

    refactor: response_model updated

--- lib/features/quiz/models/response_model.dart
+++ lib/features/quiz/models/response_model.dart
@@ -2,20 +2,24 @@ import 'dart:convert';
 
 class ResponseModel {
   final String? text;
+  final int? score;
 
   ResponseModel({
     this.text,
+    this.score,
   });
 
   factory ResponseModel.fromMap(Map<String, dynamic> map) {
     return ResponseModel(
       text: map['text'],
+      score: map['score'],
     );
   }
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{};
     map['text'] = text;
+    map['score'] = score;
     return map;
   }
 
